4cb834440804621972bfbc538ef170ec6acd1f7a
Petr Rockai <me@mornfall.net>
2014-05-29 13:07:09 +0200

    Make the dist target work without .git.

--- local.mk
+++ local.mk
@@ -1,7 +1,16 @@
 ifeq ($(MAKECMDGOALS), dist)
-  dist-files += $(shell git ls-files)
+    ifeq ($(wildcard .git),)
+      dist-files += README config/ tests/ Makefile Makefile.config.in mk/ doc/ \
+	  corepkgs/ misc/ \
+          $(shell find scripts/ -name \*.in -or -name \*.pl -or -name \*.sh) \
+          $(shell find perl/ -name \*.pm -or -name \*.xs -or -name \*.in) \
+          $(shell find -name local.mk) \
+          $(shell for s in hh cc l y h c hpp sql sdf xsl js css; do find src -name \*.$$s; done)
+    else
+        dist-files += $(shell git ls-files)
+    endif
 endif
-
+ 
 dist-files += configure config.h.in nix.spec
 
 GLOBAL_CXXFLAGS += -I . -I src -I src/libutil -I src/libstore -I src/libmain -I src/libexpr
